358616e7ebe6713804edf16b6a39ab5d68aa39e6
NITHIKA SENAWEERA <2206290@uad.ac.uk>
2024-12-23 13:47:35 +0000

    feat: Added a sample get request to the api

--- src/main/java/ns2003/github/io/GymTrack/exercise/ExerciseController.java
+++ src/main/java/ns2003/github/io/GymTrack/exercise/ExerciseController.java
@@ -1,9 +1,14 @@
 package ns2003.github.io.GymTrack.exercise;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ExerciseController
 {
+    @GetMapping("/api/exercises")
+    String home(){
+        return "Hello World";
+    }
 
 }
--- src/main/java/ns2003/github/io/GymTrack/exercise/ExerciseTempDB.java
+++ src/main/java/ns2003/github/io/GymTrack/exercise/ExerciseTempDB.java
@@ -3,6 +3,7 @@ package ns2003.github.io.GymTrack.exercise;
 import java.util.List;
 
 public class ExerciseTempDB {
+    
 
 
 }
