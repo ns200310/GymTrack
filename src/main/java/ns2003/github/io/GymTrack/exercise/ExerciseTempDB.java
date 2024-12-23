6a97f3e30cd2709b9f24233602780efc31c75a56
NITHIKA SENAWEERA <2206290@uad.ac.uk>
2024-12-23 13:58:21 +0000

    feat: Added a temporary class as db

--- src/main/java/ns2003/github/io/GymTrack/exercise/ExerciseTempDB.java
+++ src/main/java/ns2003/github/io/GymTrack/exercise/ExerciseTempDB.java
@@ -1,9 +1,22 @@
 package ns2003.github.io.GymTrack.exercise;
 
+import org.springframework.web.bind.annotation.PostMapping;
+
 import java.util.List;
 
 public class ExerciseTempDB {
-    
+    private List<Exercise> exercises;
+
+    List<Exercise> getExercises() {
+        return exercises;
+    }
+    @PostMapping
+    private void init(){
+        exercises.add(new Exercise(1, "Chin Assist", 10, 3, 20));
+        exercises.add(new Exercise(2, "Dip Assist", 10, 3, 15));
+        exercises.add(new Exercise(3, "Bicep Curl", 10, 3, 20));
+
+    }
 
 
 }
