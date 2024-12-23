5148c245c337d3d198cdd4a32067fe5de9f3ec12
NITHIKA SENAWEERA <2206290@uad.ac.uk>
2024-12-23 13:06:22 +0000

    feat: Project creation using spring initializer

---
+++ src/main/java/ns2003/github/io/GymTrack/GymTrackApplication.java
@@ -0,0 +1,13 @@
+package ns2003.github.io.GymTrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymTrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ns2003/github/io/GymTrack/GymTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package ns2003.github.io.GymTrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
